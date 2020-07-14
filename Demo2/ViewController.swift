afaa979809d31619f7fef9253869b055d5a6a6ed
Chloe Choo <chloeccy7@gmail.com>
2020-07-14 19:13:10 +0800

    Initial Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by Chloe Choo on 14/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
